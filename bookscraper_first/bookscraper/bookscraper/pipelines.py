ef2738f857a93d57d42b01bb94ec4f572d9bdd19
Richie Mugambi <richiemugambi4@gmail.com>
2023-12-23 11:19:26 +0300

    feat: added save to database

--- bookscraper_first/bookscraper/bookscraper/pipelines.py
+++ bookscraper_first/bookscraper/bookscraper/pipelines.py
@@ -34,15 +34,15 @@ class BookscraperPipeline:
             value = value.replace("£","")
             adapter[price_key]= float(value)
 
-        # Availability, split and only use the integer
-        availability_string = adapter.get("availability")
-        print(availability_string)
+        # Availability --> extract number of books in stock
+        availability_string = adapter.get('availability')
         split_string_array = availability_string.split('(')
         if len(split_string_array) < 2:
-            adapter["availability"] = 0
+            adapter['availability'] = 0
         else:
-            availability_array = split_string_array[1].split(" ")
-            adapter["availability"]=int(availability_array[0])
+            availability_array = split_string_array[1].split(' ')
+            adapter['availability'] = int(availability_array[0])
+
 
         # Reviews, converting to integer
         reviews_string = adapter.get("number_of_reviews")
@@ -67,3 +67,96 @@ class BookscraperPipeline:
             0
 
         return item
+
+
+import mysql.connector
+class SaveToMysqlPipeline:
+    # connect to mysql db
+    def __init__(self):
+        self.mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database ="books"
+        )
+
+        #using cursor, create a table
+        self.cur = self.mydb.cursor()
+
+        self.cur.execute(
+            """CREATE TABLE IF NOT EXISTS books(
+                id INT AUTO_INCREMENT NOT NULL,
+                url VARCHAR(255),
+                title VARCHAR(255),
+                product_type VARCHAR(255),
+                price_excl_tax DECIMAL(8, 2),
+                price_incl_tax DECIMAL(8, 2),
+                tax DECIMAL(8, 2),
+                availability VARCHAR(255),
+                number_of_reviews INT,
+                stars INT,
+                category VARCHAR(50),
+                description  TEXT,
+                price DECIMAL(8,2),
+                upc VARCHAR(255),
+                PRIMARY KEY(id)
+            )"""
+        )
+
+    def process_item(self, item, spider):
+        self.cur.execute(
+            """
+            INSERT INTO books(
+                url,
+                title,
+                product_type,
+                price_excl_tax,
+                price_incl_tax,
+                tax,
+                availability,
+                number_of_reviews,
+                stars,
+                category ,
+                description,
+                price,
+                upc
+            )
+            VALUES
+            (
+                %s,
+                %s,
+                %s,
+                %s,
+                %s,
+                %s,
+                %s,
+                %s,
+                %s,
+                %s,
+                %s,
+                %s,
+                %s
+            )
+            """
+            ,(
+                item["url"],
+                item["title"],
+                item["product_type"],
+                item["price_excl_tax"],
+                item["price_incl_tax"],
+                item["tax"],
+                item["availability"],
+                item["number_of_reviews"],
+                item["stars"],
+                item["category"],
+                str(item["description"][0]),
+                item["price"],
+                item["upc"],
+            )
+        )
+        self.mydb.commit()
+        return item
+    def close_spider(self):
+        self.cur.close()
+        self.mydb.close()
+
